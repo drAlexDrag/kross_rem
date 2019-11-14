4920a66dc8da9973e34a4f624f309a432b521e63
Dron <dralexdrag@gmail.com>
2019-11-14 13:48:51 +0300

    продление срока хранения резервных копий. Слияние веток

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 require "libs/rb.php";
-R::setup( 'mysql:host=localhost;dbname=kross', 'dron', 'port2100' ); //for localhost
-// R::setup( 'mysql:host=192.168.50.37;dbname=kross', 'dron', 'port2100' );
+// R::setup( 'mysql:host=localhost;dbname=kross', 'dron', 'port2100' ); //for localhost
+R::setup( 'mysql:host=192.168.50.37;dbname=kross', 'dron', 'port2100' );
 if ( !R::testConnection() )
 {
         exit ('Нет соединения с базой данных');
